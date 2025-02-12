401e02c4c1cfcf3a41acd09a12f11f9387164701
afslucass <afslucass@gmail.com>
2025-02-12 20:56:02 -0300

    featL add comments about OAuth2

--- src/routes/unprotected-routes.ts
+++ src/routes/unprotected-routes.ts
@@ -2,6 +2,12 @@ import express from "express";
 
 const router = express.Router();
 
+// Isso aqui nao existe, nao eh boa pratica usar rotas da aplicacao para trafegar credenciais, vamos usar o protocolo OAuth2
+
+// no mobile, o app ira abrir o formulario de login no browser, apos ser autenticado no formulario (keycloak, google), o browser
+
+// chamara um deeplink para o app, enviando dados como o access_token, q eh o codigo q autentica o usuario.
+
 router.get("/login", () => {});
 router.put("/create-account", () => {});
 
