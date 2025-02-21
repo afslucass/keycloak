c1d867e0de5c1201339c557ad77d7935ac5909ee
afslucass <afslucass@gmail.com>
2025-02-20 23:16:09 -0300

    temp

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import "dotenv/config";
 import express from "express";
+import session from "express-session";
 
+import { keycloak, memoryStore } from "./keycloak";
 import { errorHandler } from "./middleware/error-handler";
 import protectedRoutes from "./routes/protected-routes";
 import unprotectedRoutes from "./routes/unprotected-routes";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(bodyParser.json());
+app.use(cors());
+app.use(
+  session({
+    secret: "mySecret",
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore,
+  })
+);
 app.use(express.json());
+app.use(keycloak.middleware());
 app.use(protectedRoutes);
 app.use(unprotectedRoutes);
 app.use(errorHandler);
--- src/routes/protected-routes.ts
+++ src/routes/protected-routes.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import { keycloak } from "../keycloak";
 
 const router = express.Router();
 
-router.get("/get-user-data", () => {});
-router.post("/send-data", () => {});
-router.put("/update-data", () => {});
-router.get("/logout", () => {});
+router.get("/get-user-data", keycloak.protect(), (req, res) => {
+  res.send("hello worsd");
+});
+router.post("/send-data", keycloak.protect(), (req, res) => {
+  console.log(`DEU BOM`);
+  console.log(req.headers);
+  res.json({});
+});
+// router.put("/update-data", keycloak.protect(), () => {});
+// router.use(keycloak.middleware({ logout: "/logoff" }));
 
 export default router;
